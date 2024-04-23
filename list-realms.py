e592b6b152dd0874ea055c1a9da09056fd14e1ff
Erich (EJ) Best <20603184+ejbest@users.noreply.github.com>
2024-04-23 08:11:08 -0400

    Create list-realms.py

---
+++ list-realms.py
@@ -0,0 +1,18 @@
+import os
+import configparser
+from keycloak import KeycloakAdmin
+
+
+# Init Connection
+config = configparser.ConfigParser()
+creds_file = os.path.expanduser("~/.ssh/mycreds-master")
+config.read(creds_file)
+keycloak_admin = KeycloakAdmin(
+    server_url=config.get("myvars", "myurl"),
+    username=config.get("myvars", "myuser"),
+    password=config.get("myvars", "mypass"),
+    realm_name="master")
+
+list_realms = keycloak_admin.get_realms()
+for realm in list_realms:
+    print(realm['name'])
